9d265228c5f0d62dd85155beba99e4e8f080043a
Fabio mayorga <fabio.mayorga@leadis.co>
2018-02-05 08:56:03 -0500

    menu lateral, mapa con marcas y otros componentes

--- js/AngularJS/app.js
+++ js/AngularJS/app.js
@@ -3,8 +3,10 @@
 		[  
 
 		'mapControllers',
+		'searchNavControllers',
 
 		'toolsFactory',
+		'resultsFactory',
 		
 		'ngRoute',
 		'ngStorage',
@@ -43,7 +45,7 @@
 		protocolo : "http://",
 		nombreApi : "promee-api",
 		servidor : "",
-		puertoLead : ':NUMERO',
+		puerto : ':NUMERO',
 		app: (window.location.host).split('.')[0] 
 	})
 
@@ -69,76 +71,33 @@
 
 	app.controller('mainController', function($scope, $rootScope, $window, $route, $mdToast , $mdDialog, $mdMedia, $timeout, $location, $http , $localStorage ,$mdSidenav, $timeout, tools) {	
 
-		$scope.defaultZoom = 14;
-		
-		$scope.$on('$viewContentLoaded', function() {
+		$scope.messages = {};
+		$scope.messages.parentProperty = function(mensaje, textoBoton, duracion, target){
 
-			//hasta que no este alojada en un dominio seguro, pailas
-			if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(showPosition);
-			} else { 
-				x.innerHTML = "Geolocation is not supported by this browser.";
+			if(!textoBoton){
+				var textoBoton = "";
 			}
 
-
-			function showPosition (position) {
-				x.innerHTML = "Latitude: " + position.coords.latitude + 
-				"<br>Longitude: " + position.coords.longitude;
+			if(!duracion){
+				var duracion = 2000;
 			}
 
-			map = new google.maps.Map(document.getElementById('map'), {
-				center: {lat: 3.445869, lng: -76.531418},
-				zoom: $scope.defaultZoom ,
-				fullscreenControl: false,
-				mapTypeControl: false
-			});
-		});
+			if(!target){
+				var target = '#toast-container';
+			}
 
-		$scope.categories = ["ropa", "herramientras","juguetes"];
+			var pinTo = $scope.getToastPosition();
+			var toast = $mdToast.simple().textContent(mensaje)
+			.action(textoBoton).highlightAction(true)
+			.hideDelay(duracion).position(pinTo)
+			.parent(document.querySelectorAll(target));
 
-		$scope.categorias= (tools.randomizer($scope.categories));
-
-		$scope.toggleLeft = buildToggler('left');
-		$scope.toggleRight = buildToggler('right');
-
-		$scope.isOpenRight = function(){
-			return $mdSidenav('right').isOpen();
-		};
-
-		function buildToggler(componentId) {
-			return function() {
-				$mdSidenav(componentId).toggle();
-			};
-		}
-
-		$scope.searchProducts = function(){
-			$scope.products=["perfume", "herramientas", "comida"]
-		}
-
-		$scope.close = function () {
-
-			$mdSidenav('right').close()
-			.then(function () {
-
-			});
-		};
-
-		document.onkeypress = function(evt) {
-			evt = evt || window.event;
-			var charCode = evt.keyCode || evt.which;
-			var charStr = String.fromCharCode(charCode);
-			if (evt.keyCode == 13) {
-
-				if($scope.focus==true && $scope.products.length==0){
-					$scope.searchProducts();
+			$mdToast.show(toast).then(function(response) {
+				if (response == 'Aceptar') {
 				}
+			});
 
-			}
-		}; function code(e) {
-			e = e || window.event;
-			return(e.keyCode || e.which);
 		}
-
 
 
 	})
--- js/AngularJS/factories/tools/toolsFactory.js
+++ js/AngularJS/factories/tools/toolsFactory.js
@@ -35,13 +35,53 @@ angular.module('toolsFactory', [])
 				var temp = sourceArray[j];
 				sourceArray[j] = sourceArray[i];
 				sourceArray[i] = temp;  
-
-
 			}
-
 			return sourceArray;
+		},
 
+		starsMaker: function(rank) {
+
+			var arrayRank = [];
+
+			var t1 = Math.ceil(rank)*10;
+			var t2 = rank*10;
+
+			var tr = t1-t2;							
+
+			for(i=0; i<5; i++){
+
+				if( i < Math.floor(rank)){
+					arrayRank.push(1);
+				}
+				else if( (i) == Math.floor(rank) ){
+					if(t1==t2){
+						arrayRank.push(-1);
+					}
+					else{
+						if(tr <= 3){
+							arrayRank.push(1);
+						}
+						else if(tr > 3 && tr <=7 ){
+							arrayRank.push(0);
+						}
+						else if(tr==10){
+							arrayRank.push(-1);
+						}
+						else{
+							arrayRank.push(-1);
+						}
+					}
+					
+				}
+				else{
+					arrayRank.push(-1);
+				}
+			}	
+
+			return arrayRank;
 		}
+
+
 
 
 	}
